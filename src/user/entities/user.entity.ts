13febaba391ebe7b36ee2c4a3d1ce7e36c43f139
julie kim <julie.kim.dev@gmail.com>
2024-03-15 01:24:08 +0900

    엔티티 class-validator 추가

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,7 @@
 import { Column, CreateDateColumn, Entity, Index, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 import { Role } from '../types/userRole.type';
+import { IsEmail, IsEnum, IsString } from 'class-validator';
 
 @Index('email', ['email'], { unique: true })
 @Entity({
@@ -10,15 +11,19 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsEmail({}, { message: '이메일 형식에 맞지 않습니다.' })
   @Column({ type: 'varchar', unique: true, nullable: false })
   email: string;
 
+  @IsString()
   @Column({ type: 'varchar', select: false, nullable: false })
   password: string;
 
+  @IsString()
   @Column({ type: 'varchar', unique: false, nullable: false })
   name: string;
 
+  @IsEnum(Role)
   @Column({ type: 'enum', enum: Role, default: Role.USER }) // 기본 타입은 USER
   role: Role;
 
